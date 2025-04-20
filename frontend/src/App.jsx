815acf32e3037003efae4db5e67a8b73e0a57c1c
Ran Ram <p0038974@ness-tech.co.il>
2025-04-20 14:30:13 +0300

    fixed routing (Login at root, Signup at /signup)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Signup from './pages/Signup';
+import Login from './pages/Login';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Login />} /> {/* התחברות ראשית */}
+        <Route path="/signup" element={<Signup />} /> {/* הרשמה */}
+        <Route path="/dashboard" element={<Dashboard />} /> {/* דשבורד */}
       </Routes>
     </Router>
   );
