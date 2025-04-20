2ecd35ccce0db47695d3a53c1a1e8becdf0a4919
Ran Ram <p0038974@ness-tech.co.il>
2025-04-20 12:52:47 +0300

    force add vite.config.js for Vercel deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
+import 'dotenv/config'
+
 
 // טוען משתני סביבה מקובץ .env
 dotenv.config()
