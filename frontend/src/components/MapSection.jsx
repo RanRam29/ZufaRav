3e475c40fd25a967bf30c138368ed40793e85a5e
Ran Ram <p0038974@ness-tech.co.il>
2025-04-27 16:06:52 +0300

    fix: password comparison using bcrypt correctly

--- frontend/src/components/MapSection.jsx
+++ frontend/src/components/MapSection.jsx
@@ -1,4 +1,23 @@
 import { MapContainer, TileLayer, Marker, Popup, Circle } from "react-leaflet";
+import L from "leaflet";
+
+// אייקון סיכה ירוקה למשתמש
+const userIcon = new L.Icon({
+  iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowUrl: "https://unpkg.com/leaflet@1.9.3/dist/images/marker-shadow.png",
+  shadowSize: [41, 41],
+});
+
+// אייקון דגל אדום לאירועים
+const eventIcon = new L.Icon({
+  iconUrl: "https://cdn-icons-png.flaticon.com/512/684/684908.png", // אייקון דגל אדום קטן
+  iconSize: [30, 30],
+  iconAnchor: [15, 30],
+  popupAnchor: [0, -30],
+});
 
 export default function MapSection({ userLocation, events, newEventIds = [] }) {
   console.debug("🗺️ Rendering MapSection...", { userLocation, numberOfEvents: events?.length || 0 });
@@ -12,7 +31,7 @@ export default function MapSection({ userLocation, events, newEventIds = [] }) {
     <MapContainer
       center={[31.8, 35.2]}
       zoom={8}
-      scrollWheelZoom={true}   // ✅ אפשרות Zoom בגלגלת העכבר
+      scrollWheelZoom={true}
       style={{ height: '400px', width: '100%' }}
     >
       <TileLayer
@@ -22,7 +41,10 @@ export default function MapSection({ userLocation, events, newEventIds = [] }) {
 
       {userLocation && (
         <>
-          <Marker position={[userLocation.lat, userLocation.lng]}>
+          <Marker
+            position={[userLocation.lat, userLocation.lng]}
+            icon={userIcon}
+          >
             <Popup>המיקום שלך</Popup>
           </Marker>
           <Circle center={[userLocation.lat, userLocation.lng]} radius={100} pathOptions={{ color: 'blue' }} />
@@ -32,7 +54,7 @@ export default function MapSection({ userLocation, events, newEventIds = [] }) {
 
       {events.map((e, i) => (
         e.lat && e.lng ? (
-          <Marker key={i} position={[e.lat, e.lng]}>
+          <Marker key={i} position={[e.lat, e.lng]} icon={eventIcon}>
             <Popup>
               <strong>{e.title}</strong><br />
               {e.address}<br />
